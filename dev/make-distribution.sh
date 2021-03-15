5056445bed01553a57c53f4785713f772f09ca86
Rickard Lundin <rickard@ignalina.dk>
2021-03-15 18:20:53 +0100

    mem

--- dev/make-distribution.sh
+++ dev/make-distribution.sh
@@ -161,7 +161,7 @@ fi
 # Build uber fat JAR
 cd "$SPARK_HOME"
 
-export MAVEN_OPTS="${MAVEN_OPTS:--Xmx31g -Xms31g -XX:ReservedCodeCacheSize=2g}"
+export MAVEN_OPTS="${MAVEN_OPTS:--Xmx31g  -XX:ReservedCodeCacheSize=2g}"
 
 # Store the command as an array because $MVN variable might have spaces in it.
 # Normal quoting tricks don't work.
